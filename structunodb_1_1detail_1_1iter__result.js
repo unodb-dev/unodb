f728ee09a09799735bdb4ebe60fb0c56a27d0c0e
laurynas-biveinis <laurynas-biveinis@users.noreply.github.com>
2026-09-06 17:14:42 +0000

    deploy: 9a43ad02f63219c8987b15d7e0469f5ba76beeeb

--- structunodb_1_1detail_1_1iter__result.js
+++ structunodb_1_1detail_1_1iter__result.js
@@ -2,8 +2,8 @@ var structunodb_1_1detail_1_1iter__result =
 [
     [ "node_ptr", "structunodb_1_1detail_1_1iter__result.html#ab22270fae893b795cee9f4d663950f41", null ],
     [ "child_index", "structunodb_1_1detail_1_1iter__result.html#a985b95e52360b47cc69e53c0ab48946e", null ],
+    [ "is_packed_value", "structunodb_1_1detail_1_1iter__result.html#a66070f404623bf5d6632da65ce899528", null ],
     [ "key_byte", "structunodb_1_1detail_1_1iter__result.html#af629f13328e6b7ebd271a9939d7559a8", null ],
     [ "node", "structunodb_1_1detail_1_1iter__result.html#a854585e8656a3b42ae0e48a97d2f970a", null ],
-    [ "packed_leaf", "structunodb_1_1detail_1_1iter__result.html#adea10db562217d81523ea9c0c6464e9e", null ],
     [ "prefix", "structunodb_1_1detail_1_1iter__result.html#a567edf1377659f0138ac433674cf6091", null ]
 ];
